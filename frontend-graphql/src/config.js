c717865e99eb3417d3bbe607b51caf11bd2c8c7c
Vashisth00 <vashisthbhushan@gmail.com>
2020-11-02 04:09:19 +0530

    Config changes

--- frontend-graphql/src/config.js
+++ frontend-graphql/src/config.js
@@ -1,9 +1,9 @@
 // graphql api url
-let url = 'http://localhost:8080/graphql';
+let url = 'https://beerbiceps2.adesignguy.co/graphql';
 
 // If we're running on Docker, use the WordPress container hostname instead of localhost.
 if (process.env.HOME === '/home/node') {
-  url = 'http://wp-headless:8080/graphql';
+  url = 'https://beerbiceps2.adesignguy.co/graphql';
 }
 const Config = {
   gqlUrl: url,
--- frontend/config.js
+++ frontend/config.js
@@ -1,8 +1,8 @@
-let wpUrl = 'http://localhost:8080/wp-json';
+let wpUrl = 'http://http://beerbiceps2.adesignguy.co/wp-json';
 
 // If we're running on Docker, use the WordPress container hostname instead of localhost.
 if (process.env.HOME === '/home/node') {
-  wpUrl = 'http://wp-headless:8080/wp-json';
+  wpUrl = 'http://beerbiceps2.adesignguy.co/wp-json';
 }
 const Config = {
   apiUrl: wpUrl,
